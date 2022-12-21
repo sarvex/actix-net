72c1cb72652f25cac6d59f8905ac3cd6b8fba07c
JohnTitor <huyuumi.dev+love@gmail.com>
2022-12-21 02:47:08 +0000

    Deploying to gh-pages from  @ fe38312db062264960676816e26f9295ff878d66 🚀

--- implementors/serde/ser/trait.Serialize.js
+++ implementors/serde/ser/trait.Serialize.js
@@ -1,3 +1,3 @@
 (function() {var implementors = {
-"bytestring":[["impl <a class=\"trait\" href=\"https://docs.rs/serde/1.0.147/serde/ser/trait.Serialize.html\" title=\"trait serde::ser::Serialize\">Serialize</a> for <a class=\"struct\" href=\"bytestring/struct.ByteString.html\" title=\"struct bytestring::ByteString\">ByteString</a>"]]
+"bytestring":[["impl <a class=\"trait\" href=\"https://docs.rs/serde/1.0.151/serde/ser/trait.Serialize.html\" title=\"trait serde::ser::Serialize\">Serialize</a> for <a class=\"struct\" href=\"bytestring/struct.ByteString.html\" title=\"struct bytestring::ByteString\">ByteString</a>"]]
 };if (window.register_implementors) {window.register_implementors(implementors);} else {window.pending_implementors = implementors;}})()
